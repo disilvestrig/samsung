c8d39c28d7b30665f08fc4964fef10c80bc7da6a
Giulio Di Silvestri <42960623+disilvestrig@users.noreply.github.com>
2021-03-23 19:53:26 +0100

    Add files via upload

---
+++ mobile.js
@@ -0,0 +1,28 @@
+let changeimg = () => {
+    
+    document.getElementById("jsprova").src = "./phone2.jpg"
+
+}
+
+let changeimg2 = () => {
+    document.getElementById("jsprova").src = "./phone1.jpg"
+}
+
+let insertPass = () => {
+if (document.getElementById("jsprova").src == "file:///Users/giulio/server-verifica/phone2.jpg"){
+let x = 0
+let password = prompt("Insert password")
+while (password != 1234){
+    alert("Wrong password")
+    password = prompt("Insert password")
+    x++ 
+    if (x == 5){
+        alert("The phone is blocked refresh the page")
+        break
+    }   
+}
+if ( +password == 1234){
+    document.getElementById("jsprova").src = "./phone3.jpg"
+}
+}  
+}
