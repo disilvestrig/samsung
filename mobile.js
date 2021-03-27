075334f6697412058d80df0292b95a15fba04901
Giulio Di Silvestri <42960623+disilvestrig@users.noreply.github.com>
2021-03-27 09:02:04 +0100

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -1,4 +1,4 @@
-let on = 0
+let on = 1
 let changeimg = () => {
     
     document.getElementById("jsprova").src = "./phone2.jpg"
@@ -14,18 +14,19 @@ let changeimg2 = () => {
 let insertPass = () => {
 
 if (on == 2){
-let x = 0
-let password = prompt("Insert password")
-while (password != 1234){
-    alert("Wrong password")
-    password = prompt("Insert password")
-    x++ 
-    if (x == 5){
-        alert("The phone is blocked refresh the page")
-        break
+    let x = 0
+    let password = prompt("Insert password")
+    while (password != 1234){
+        alert("Wrong password")
+        password = prompt("Insert password")
+        x++ 
+        if (x == 5){
+            alert("The phone is blocked refresh the page")
+            break
     }   
 }
 if ( +password == 1234){
+    on = 1
     document.getElementById("jsprova").src = "./phone3.jpg"
 }
 }  
